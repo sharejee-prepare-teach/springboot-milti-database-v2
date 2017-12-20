06ad07b61586a8ae47c1866c94e126852c883d91
Rith <rithronlkh@gmail.com>
2017-12-20 16:48:50 +0700

    add configure muliple database and restcontroller

---
+++ src/main/java/com/roufid/tutorial/entity/mysql/Author.java
@@ -0,0 +1,68 @@
+package com.roufid.tutorial.entity.mysql;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "AUTHOR")
+public class Author implements Serializable {
+
+	private static final long serialVersionUID = -1848119459950659679L;
+
+	@Id
+	@Column(name = "ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	@Column(name = "FIRSTNAME")
+	private String firstname;
+
+	@Column(name = "LASTNAME")
+	private String lastname;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the firstname
+	 */
+	public String getFirstname() {
+		return firstname;
+	}
+
+	/**
+	 * @param firstname
+	 *            the firstname to set
+	 */
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	/**
+	 * @return the lastname
+	 */
+	public String getLastname() {
+		return lastname;
+	}
+
+	/**
+	 * @param lastname
+	 *            the lastname to set
+	 */
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+}
---
+++ src/main/java/com/roufid/tutorial/entity/postgresql/Book.java
@@ -0,0 +1,64 @@
+package com.roufid.tutorial.entity.postgresql;
+
+import java.io.Serializable;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "BOOK")
+public class Book implements Serializable {
+	
+	private static final long serialVersionUID = -9019470250770543773L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	@Column
+	private String name;
+	
+	@Column
+	private Long authorId;
+
+	/**
+	 * @return the id
+	 */
+	public Long getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the authorId
+	 */
+	public Long getAuthorId() {
+		return authorId;
+	}
+
+	/**
+	 * @param authorId the authorId to set
+	 */
+	public void setAuthorId(Long authorId) {
+		this.authorId = authorId;
+	}
+}
